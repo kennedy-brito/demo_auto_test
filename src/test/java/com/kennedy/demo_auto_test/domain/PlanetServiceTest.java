27d7193de96ca81e445c275c3da7d2d39ed76f7d
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-21 09:51:40 -0300

    test: get planet by id

--- src/test/java/com/kennedy/demo_auto_test/domain/PlanetServiceTest.java
+++ src/test/java/com/kennedy/demo_auto_test/domain/PlanetServiceTest.java
@@ -2,13 +2,17 @@ package com.kennedy.demo_auto_test.domain;
 
 import static com.kennedy.demo_auto_test.common.PlanetConstants.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 //@SpringBootTest(classes = PlanetService.class)
@@ -45,7 +49,24 @@ public class PlanetServiceTest {
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
 
         assertThatThrownBy( () -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
 
+    @Test
+    public void getPlanet_ByExistingId_ReturnPlanet(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
 
+        Optional<Planet> sut = planetRepository.findById(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByNonExistingId_ReturnEmpty(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.findById(1L);
+
+        assertThat(sut).isEmpty();
     }
 }
