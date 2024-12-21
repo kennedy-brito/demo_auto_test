6d20b6d3e71383404ee31f6aeba65245e2cfcee3
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-21 09:15:07 -0300

    Testing when a unit has to throw an exception, it should be noted that normally, a unit doesn't treat an exception, its the controller that does that. A unit merely relaunch the exception

--- src/test/java/com/kennedy/demo_auto_test/common/PlanetConstants.java
+++ src/test/java/com/kennedy/demo_auto_test/common/PlanetConstants.java
@@ -5,4 +5,5 @@ import com.kennedy.demo_auto_test.domain.Planet;
 public class PlanetConstants {
 
     public static final Planet PLANET = new Planet(null, "name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet(null, "", "", "");
 }
--- src/test/java/com/kennedy/demo_auto_test/domain/PlanetServiceTest.java
+++ src/test/java/com/kennedy/demo_auto_test/domain/PlanetServiceTest.java
@@ -1,7 +1,7 @@
 package com.kennedy.demo_auto_test.domain;
 
 import static com.kennedy.demo_auto_test.common.PlanetConstants.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -9,10 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ExtendWith(MockitoExtension.class)
 //@SpringBootTest(classes = PlanetService.class)
@@ -41,5 +37,15 @@ public class PlanetServiceTest {
 
         // * ASSERT
         assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy( () -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+
+
     }
 }
