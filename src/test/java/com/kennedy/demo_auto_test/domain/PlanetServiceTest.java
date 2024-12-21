3b03d205bb6c48c63178edf983197c61992dac69
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-21 09:08:54 -0300

    Improving performance of test of create planet using mockito instead of spring for tests

--- src/test/java/com/kennedy/demo_auto_test/domain/PlanetServiceTest.java
+++ src/test/java/com/kennedy/demo_auto_test/domain/PlanetServiceTest.java
@@ -5,26 +5,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
 
-    @Autowired
+//    @Autowired
+    @InjectMocks
     private PlanetService planetService;
 
-    @MockitoBean
+//    @MockitoBean
+    @Mock
     private PlanetRepository planetRepository;
 
     //Test Name Pattern: operation_stateOrParams_Return
     @Test
     public void createPlanet_withValidData_ReturnsPlanetStatus201(){
 
-//        * AAA PRINCIPLE
-//        * ARRANGE
+        // * AAA PRINCIPLE
+        // * ARRANGE
 
         when(planetRepository.save(PLANET)).thenReturn(PLANET);
 
