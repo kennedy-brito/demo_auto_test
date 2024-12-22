8bd4fe5f7d5d569d99df78f0d06213ca72b62ec1
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-22 18:17:29 -0300

    preventing a static class constructor of being analyzed in the coverage test

--- src/main/java/com/kennedy/demo_auto_test/domain/QueryBuilder.java
+++ src/main/java/com/kennedy/demo_auto_test/domain/QueryBuilder.java
@@ -1,8 +1,11 @@
 package com.kennedy.demo_auto_test.domain;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class QueryBuilder {
 
     public static Example<Planet> makeQuery(Planet planet){
